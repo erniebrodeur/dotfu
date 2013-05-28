90afb0b6f53b25b45e23825ead6197cfc7d7111f
Ernie Brodeur <ebrodeur@ujami.net>
2013-05-28 03:50:37 -0700

    Made my attribute methods smarter.  Added config_file

--- lib/dotfu/repos.rb
+++ lib/dotfu/repos.rb
@@ -2,6 +2,7 @@ module Dotfu
   class Repos
     attr_accessor :repo
     attr_accessor :user
+    attr_accessor :config_file
     attr_accessor :working_dir
 
 
@@ -12,18 +13,19 @@ module Dotfu
 
     ### Specialized attribute methods
 
+    # return a config_file (if overriden) or the default of 'dotfu.json'
+    def config_file
+      return @config_file ? @config_file : "dotfu.json"
+    end
+
     # prepend repo with dotfiles- if it doesn't exist as it is set.
     def repo=(word)
-      if word.start_with? 'dotfiles-'
-        @repo = word
-      else
-        @repo = "dotfiles-#{word}"
-      end
+      return @repo = word.start_with?('dotfiles-') ? word : "dotfiles-#{word}"
     end
 
     # return user or the user in the config file.
     def user
-      return Dotfu.config_user if !@user
+      return @user ? @user : Dotfu.config_user
     end
 
     # return the explicit directory this repo is cloned into.

58b4c7d42b48ffd9849ba4993bd4bd8abe303ecd
Ernie Brodeur <ebrodeur@ujami.net>
2013-05-29 02:24:00 -0700

    Fixed a few minor bugs, if it fails it'll at least tell us why.

--- commands/list.rb
+++ commands/list.rb
@@ -1,7 +1,7 @@
 command 'list' do
   banner "List the repos of a given user."
   run do |opts, args|
-    if args
+    if args.any?
       username = args.first
     else
       username = Dotfu.config_user
@@ -10,10 +10,10 @@ command 'list' do
     begin
       results = Dotfu::GITHUB.repos.list(user:username).select {|r| r.name.start_with? 'dotfiles' };
     rescue Exception => e
-      puts 'nogojoe'
+      puts e
     end
 
-    if results.empty?
+    unless results && results.any?
       puts "No suitable repositories found."
     else
       puts "Repositories for #{username}:"
